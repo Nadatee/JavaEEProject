d0ff2fd81c3fe42c224cdae60f81d64504359947
Nadatee <nadatee@hotmail.com>
2018-03-19 12:44:17 +0100

    Read from Excel file

---
+++ javaEEProject.ejb/ejbModule/beans/POIBean.java
@@ -0,0 +1,47 @@
+package beans;
+import java.io.IOException;
+import java.net.URL;
+
+import javax.ejb.LocalBean;
+import javax.ejb.Schedule;
+import javax.ejb.Stateless;
+
+import org.apache.poi.hssf.usermodel.HSSFWorkbook;
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.ss.usermodel.Sheet;
+import org.apache.poi.ss.usermodel.Workbook;
+/**
+ * Session Bean implementation class POIBean
+ */
+@Stateless
+@LocalBean
+public class POIBean implements POIBeanRemote, POIBeanLocal {
+
+    /**
+     * Default constructor. 
+     */
+    public POIBean() {
+    	
+    }
+    
+//	@Schedule(hour="*",minute="*",second="*/30")
+	@Schedule(hour="*",minute="*/10")
+	
+	public void readExcel() throws IOException {
+		String url = "https://openfigi.com/assets/local/exchange-code-mic-mapping.xls";
+		URL exchange = new URL(url);
+		Workbook wb = new HSSFWorkbook(exchange.openStream());
+		Sheet sheet = wb.getSheetAt(0);
+		for (Row row : sheet) {
+			System.out.println("");
+			for (Cell cell : row) {
+	            System.out.print(" - ");
+	            String text = cell.getStringCellValue();
+	            System.out.print(text);
+			}
+		}
+		wb.close();
+
+	}
+}
---
+++ javaEEProject.ejbClient/ejbModule/beans/POIBeanLocal.java
@@ -0,0 +1,11 @@
+package beans;
+
+import java.io.IOException;
+
+import javax.ejb.Local;
+
+@Local
+public interface POIBeanLocal {
+	
+	public void readExcel() throws IOException;
+}
